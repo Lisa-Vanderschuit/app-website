f676cb1b24ef80b544ceb2093ecf9fbd9f4e7626
Lisa-Villeneuve <lisav@Lisa-Villeneuves-MacBook-Pro.local>
2014-02-13 10:25:11 -0500

    fixed code

--- js/ajax.js
+++ js/ajax.js
@@ -3,7 +3,7 @@ var revData = $.ajax('js/testimonials.json', {
 });
 
 revData.done(function (data) {
-	var rev = Math.round(Math.random() * 2 );
+	var rev = Math.round( Math.random() * (data.length - 1) );
 
 	$('.rev-name').html(data[rev].name);
 	$('.rev-review').html(data[rev].review);
